212ce913300bbb92d1d550e0e6b8cff9ca6de74f
YairSmadar <yairsemama@gmail.com>
2023-08-04 08:38:12 +0300

    add Segformer

---
+++ SegFormer/download_city.sh
@@ -0,0 +1,2 @@
+wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=bitoc&password=Kxnnukp3!&submit=Login' https://www.cityscapes-dataset.com/login/
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
